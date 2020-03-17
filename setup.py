15e86fd5f06f4c0d5d223058bf981239b2552c1e
Tyler Yep <tyep@stanford.edu>
2020-03-16 23:22:40 -0700

    license

--- setup.py
+++ setup.py
@@ -1,3 +1,4 @@
+import setuptools
 from setuptools import setup
 
 # name is the package name, tweeter in my example
@@ -6,12 +7,24 @@ from setuptools import setup
 # author is of course the package's author
 # packages is the name of your package
 # install_requires is a list of dependencies for the package, much like "dependencies" in package.json
+with open("README.md") as f:
+    long_description = f.read()
+
 
 setup(
-    name="ml-toolkit",
+    name="ai-toolkit",
     version="0.0.1",
-    description="ML Toolkit",
     author="Tyler Yep",
-    packages=["tweeter"],
-    install_requires=["tweepy>=3.8.0"],
+    author_email="tyep10@gmail.com",
+    description="AI Toolkit",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/tyleryep/ai-toolkit",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
 )
