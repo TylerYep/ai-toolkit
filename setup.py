1f2cf7896e8b2ad33e59b68e0e81f21b27bd350d
Tyler Yep <tyep@cs.stanford.edu>
2020-10-27 23:35:30 -0700

    convert setup.py to metadata

--- setup.py
+++ setup.py
@@ -1,22 +1,3 @@
-import setuptools
+from setuptools import setup
 
-with open("README.md") as f:
-    LONG_DESCRIPTION = f.read()
-
-setuptools.setup(
-    name="ai-toolkit",
-    version="0.0.1",
-    author="Tyler Yep",
-    author_email="tyep@cs.stanford.edu",
-    description="AI Toolkit",
-    long_description=LONG_DESCRIPTION,
-    long_description_content_type="text/markdown",
-    url="https://github.com/tyleryep/ai-toolkit",
-    packages=setuptools.find_packages(),
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
-    python_requires=">=3.7",
-)
+setup()
